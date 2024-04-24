6b5bf06ff6e7440d1314cc9116b57023674212b9
FarsanBaloo <106886894+FarsanBaloo@users.noreply.github.com>
2024-04-24 11:47:17 +0200

    Update Ritchytetestcorner.py

--- Lab4/Ritchytetestcorner.py
+++ Lab4/Ritchytetestcorner.py
@@ -2,76 +2,85 @@ import numpy as np
 
 class agent:
     def __init__(self):
-        self.BuyList = np.array([3,2,1])
+        self.inventory = np.random.randint(0, 10, size=7)
 
  
 class bauhaus:
     def __init__(self):
-        self.inventory = np.array([2,1,0], dtype="int32")
+        self.inventory = np.array([5,4,3,2,1,0,1], dtype="int32")
     
 
-def BauhausShoppingHybrid(agenten, bauhausen):
-    maxbuy = 10
-    crossOverProbability = 0.99
+def Buy(agenten, bauhausen):
+    # Har inte funktionaliten att försöka köpa mindre om agenten inte har råd 
+    maxbuy = np.array([10, 10, 10, 10, 10, 10, 10])
+    crossOverProbability = 0.6
     
-    agentInventory = agenten.BuyList.copy()
-    print("Agentens buy lista:", agentInventory)
-    
+    agentInventory = agenten.inventory.copy()    
     bauhausInventory = bauhausen.inventory.copy()
-    print("Bauhaus inventory:", bauhausInventory)
-  
+    print(f"Agentens buy lista: {agentInventory}, Bauhaus inventory: {bauhausInventory}")
     
-     
-    # kontrollera vad agenten vill ha och vad som finns på bauhaus hyllan     
-    canBuyElement = np.where((bauhausInventory > 0))[0]
-    print("Agenten vill köpa:", canBuyElement)
-    
-    # finns det något att köpa? om inte avsluta
-    if not canBuyElement.size:
-        print("tomt på hyllan hos bauhas")
+    decisions = np.zeros(7, dtype=bool)
+    canBuyComponent = np.where(bauhausInventory > 0)[0]
+    decisions[canBuyComponent] = np.random.rand(len(canBuyComponent)) < crossOverProbability
+
+ 
+    byAmount = np.minimum(bauhausInventory, maxbuy)
+    print(f"Agenten kan köpa komponenterna: {byAmount} med hänsyn till Bauhaus lager och max begränsning")
+    print(f"Agenten köper slumpmässigt: {decisions}")
+
+    if not decisions.any():
+        print("Inga köp genomfördes på grund av  slut i bauhhaus lager")
         return agentInventory, bauhausInventory
-       
+    
+    choosenAmounts = np.random.randint(1, byAmount[decisions] + 1)
+    buyAmounts = np.zeros(len(bauhausInventory), dtype=np.int32)
+    buyAmounts[decisions] = choosenAmounts
 
-    # uppskatta max antal som kan handlas baserat på bauhaus lager
-    canByAmount = np.min(agentInventory[canBuyElement], bauhausInventory[canBuyElement])
-    print("Agenten kan köpa max antal antal baserat på bauhasus lager:", canByAmount)
     
-    # begräns yterligare antal baserat på maxbuy
-    canByAmount = np.min(canByAmount, maxbuy)
-    print("Agenten kan köpa max antal antal baserat på begränsning av maxbuy:", canByAmount)
-
-
-    # slumpa vilken vara att köpa ifrån köplistan baserat på Proben
-    toBuyElement = np.random.rand(canBuyElement.size) < crossOverProbability
-    print("Agenten vill köpa element:", toBuyElement)
-    print("Agenten vill köpa element:", toBuyElement)
-    buyElement = canBuyElement[toBuyElement]
-    print("Agenten vill köpa element:", buyElement)
+    print(f"Agent inventory innan köp: {agentInventory}")
+    agentInventory += buyAmounts
+    print("Agenten köper antalet:", buyAmounts)
+    print(f"Agent inventory efter köp: {agentInventory}")
     
-    # slumpa antal att köpa inom intervallet 1 till maxbuy
-    buy_amounts = np.random.randint(1, canByAmount[toBuyElement] + 1)
-    print("Agenten köper antal:", buyElement)
-    
-    # Genomför köp
-    print("Agent inventory innan köp:", agentInventory)
-    agentInventory[buyElement] += buy_amounts
-    print("Agent inventory efter köp:", agentInventory)
-    
-    print("Bauhaus inventory innan köp:", bauhausInventory)
-    bauhausInventory[buyElement] -= buy_amounts
-    print("Bauhaus inventory efter köp:", bauhausInventory)
+    print(f"Bauhaus inventory innan köp: {bauhausInventory}")
+    bauhausInventory -= buyAmounts
+    print(f"Bauhaus inventory efter köp: {bauhausInventory}")
 
     return agentInventory, bauhausInventory
+
+
+def Trade(agent1, agent2):
+    crossOverProbability = 0.6
+    
+    agent1Inventory = agent1.inventory.copy()    
+    agent2Inventory = agent2.inventory.copy()
+    print(f"Agent 1 inventory: {agent1Inventory}")
+    print(f"Agent 2 inventory: {agent2Inventory}")
+    
+    
+    # generate crossover condition array True/False for each  gen depending on the crossover probability vectorized
+    crossoverCondition = (np.random.rand(7) < crossOverProbability)
+    print(f'crossover condition: {crossoverCondition}')
+
+    # Produce the Offsprings and select each gen based on parent 1 or 2 depending on crossoverCondition vectorized
+    offspring1 = np.where(crossoverCondition, agent2Inventory, agent1Inventory)
+    offspring2 = np.where(crossoverCondition, agent1Inventory, agent2Inventory)
+    
+    print(f"Offspring 1: {offspring1}") 
+    print(f"Offspring 2: {offspring2}")
+        
+    
+
+    return offspring1, offspring2
     
     
 if __name__ == "__main__":
-    agenten = agent()
+    agent1 = agent()
+    agent2 = agent()
     Bauhausen = bauhaus() 
-    print(isinstance(agenten, agent))
-    
-     
 
-    agentinventory, bauhausinventory = BauhausShoppingHybrid(agenten, Bauhausen)
+        
+        
 
-    print("Agent new inventory:", agentinventory)
-    print("Bauhaus new inventory:", bauhausinventory)
+    agentinventory, bauhausinventory = Trade(agent1, agent2)
+
